7468a65fbe7c1292bf211fe44aaf6cd6accbc88c
Tami <merthiutami@gmail.com>
2023-05-30 21:50:55 +0800

    ecommerce

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        try{
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8117),0);
+            httpserver.createContext("/", new Server());
+//            test
+            httpserver.setExecutor(Executors.newSingleThreadExecutor());
+            httpserver.start();
+            System.out.println( "Listening on port 8117...");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
